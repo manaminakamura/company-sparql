bd836cdc79ea8bb7cb8ae98326c09191e4300707
Manami Nakamura <manaminakamura812@gmail.com>
2017-12-26 17:09:47 +0900

    add tse_companies

--- dbpedia/dbpedia.rb
+++ dbpedia/dbpedia.rb
@@ -3,11 +3,27 @@ require 'sparql/client'
 module Dbpedia
   PREFIXES = "
     PREFIX dbp:<http://ja.dbpedia.org/resource/>
-    PREFIX dbp-owl:<http://dbpedia/ontology/>
+    PREFIX dbp-owl:<http://dbpedia.org/ontology/>
   "
 
   def get_client
     SPARQL::Client.new('http://ja.dbpedia.org/sparql')
+  end
+
+  def tse_companies(client=nil, limit=100)
+    client ||= get_client
+    query_string = "
+      #{PREFIXES}
+      SELECT DISTINCT ?name ?abstract ?number_of_employees
+      WHERE {
+        ?company dbp-owl:wikiPageWikiLink <http://ja.dbpedia.org/resource/Category:東証一部上場企業> .
+        ?company rdfs:label ?name .
+        ?company dbp-owl:abstract ?abstract .
+        ?company dbp-owl:numberOfEmployees ?number_of_employees .
+      }
+      LIMIT #{limit}
+    "
+    results = client.query(query_string)
   end
 
   def ping
@@ -25,7 +41,11 @@ end
 
 if $0 == __FILE__
   include Dbpedia
-  res = ping
-  p res
+  res = tse_companies
+  res.each do |item|
+    p item.to_h[:name].to_s
+    p item.to_h[:abstract].to_s
+    p item.to_h[:number_of_employees].to_s
+  end
 end
 
